eeb430c88067c9ae32e2cd626bc8606bf101d825
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-28 22:45:42 +0530

    sidebar header and search add

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,19 +1,44 @@
 import React from 'react';
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+
 import './Sidebar.css';
+import { SearchOutlined } from '@material-ui/icons';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebar__header">
+                <Avatar />
 
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                    
+                    
+                    
+                </div>
             </div>
 
             <div className="sidebar__search">
-
+                <SearchIcon />
+                <input placeholder="chats" type="text" />
             </div>
 
             <div className="sidebar__chats">
-                
+
             </div>
             
         </div>
