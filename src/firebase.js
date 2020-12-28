734b848ada1a035a2b288f214ec03ad3e1eb79a5
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-28 20:21:20 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA7CavGkqIBwayQ8bJ-VNLqlGaPfCbArUk",
+    authDomain: "whatsapp-clone-1bf30.firebaseapp.com",
+    projectId: "whatsapp-clone-1bf30",
+    storageBucket: "whatsapp-clone-1bf30.appspot.com",
+    messagingSenderId: "425021167175",
+    appId: "1:425021167175:web:f554039f8807d970ccc362",
+    measurementId: "G-GDFQE632E7"
+  };
