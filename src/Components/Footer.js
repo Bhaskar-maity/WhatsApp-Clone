97dd29af607d1062f0a3f2fe42866b54d086667e
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-31 20:31:00 +0530

    minor change

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,13 +9,13 @@ function Footer() {
         <a className="" href="https://github.com/Bhaskar-maity">
           <GitHubIcon className="github__logo" />
         </a>
-        <h3>Made By Bhaskar Maity | 2020</h3>
+        <h3>Made By Bhaskar Maity</h3>
         <p>
           <strong>Made With React</strong>
         </p>
         <br/>
         <p>
-          I do not own WhatsApp brand & logo, this is for learnig purpose.
+          I don't own WhatsApp brand & logo, this is for learnig purpose.
         </p>
       </div>
     </div>
